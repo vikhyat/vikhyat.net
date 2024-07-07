abe1e9ca8effc266c9ca36dd3c5d8d7d2df74248
vik <vikhyatk@gmail.com>
2024-07-07 16:47:39 -0700

    new homepage

--- babushka-deps/vikhyat-net.rb
+++
@@ -1,8 +0,0 @@
-dep 'vikhyat-net' do
-  requires 'iptables configured'
-  requires 'nginx running'
-  requires 'mysql running'
-  requires 'thin running'
-  requires 'ghost running'
-  requires 'postfix running'
-end
